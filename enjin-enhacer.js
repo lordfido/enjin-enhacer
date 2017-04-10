1609092e1d5e23ef09ebe3eff6da90959846780e
Rafael Pérez García <rafael.perez@imperdiblesoft.com>
2017-04-10 17:02:19 +0200

    Updating enjin-enhacer with optional enhacements

--- enjin-enhacer.js
+++ enjin-enhacer.js
@@ -5,11 +5,23 @@
 /******************************************************/
 /*
   INLINE SCRIPT
-  <script src="http://lordfido.github.io/enjin-enhacer/enjin-enhacer.js"></script>
+  <script
+    src="https://lordfido.github.io/enjin-enhacer/enjin-enhacer.js"
+    id="enjin-enhacer"
+    region="Your region"
+    realm="Your realm"
+    guild="Your guild name"
+    enhacements="tags,flash-header,wowprogress-link"   // Any of these elements can be removed
+  ></script>
 
   DYNAMIC SCRIPT
   var enjinEnhacer = document.createElement("script");
-  enjinEnhacer.src = "http://lordfido.github.io/enjin-enhacer/enjin-enhacer.js";
+  enjinEnhacer.id="enjin-enhacer"
+  enjinEnhacer.src = "https://lordfido.github.io/enjin-enhacer/enjin-enhacer.js";
+  enjinEnhacer.region = "Your region";
+  enjinEnhacer.realm = "Your realm";
+  enjinEnhacer.guild = "Your guild name";
+  enjinEnhacer.enhacements = "tags,flash-header,wowprogress-link";   // Any of these elements can be removed
   document.head.appendChild(enjinEnhacer);
 */
 
@@ -22,6 +34,16 @@
   }
 
   var Enhacer = function() {
+    var elem = document.getElementById('enjin-enhacer');
+    this.region = elem.region;
+    this.realm = elem.realm;
+    this.guild = elem.guild;
+
+    var TAGS = 'tags';
+    var FLASH_HEADER = 'flash-header';
+    var WOWPROGRESS_LINK = 'wowprogress-link';
+    this.enhacements = elem.enhacements.split(',');
+
     var that = this;
 
     // Point where everything is gonna be mount
@@ -30,6 +52,13 @@
       enhacer: 'https://lordfido.github.io/enjin-enhacer/enjin-enhacer.js',
       repo: 'https://www.github.com/lordfido/enjin-enhacer/'
     };
+
+    this.parseRealm = function(realm) {
+      return realm
+        .replace("'", '-')
+        .replace(" ", '-')
+        .toLowerCase();
+    }
 
     // Add enhaced CSS
     this.loadNewStyles = function() {
@@ -43,22 +72,6 @@
       log('enhacedStyles has been loaded.');
     }
     
-    // Remove 4 layers that are blocking Flash elements to be clickable (and playable) on website's header
-    this.unblockFlashHeader = function() {
-      var elem;
-      elem = document.querySelector('.special_container .m_header .tl');
-      if (elem && elem.remove) elem.remove();
-
-      elem = document.querySelector('.special_container .m_header .tr');
-      if (elem && elem.remove) elem.remove();
-
-      elem = document.querySelector('.special_container .m_header .bl');
-      if (elem && elem.remove) elem.remove();
-
-      elem = document.querySelector('.special_container .m_header .br');
-      if (elem && elem.remove) elem.remove();
-    }
-    
     // Improve "descriptive titles" for enjin microtags
     this.increaseMicroTags = function() {
       var microTags = document.getElementsByClassName('icon_microtag');
@@ -69,9 +82,46 @@
       }
     }
     
+    // Place a link announcing Enjin Enhacer
     this.enhacedLink = function() {
       var footer = document.querySelector('#page-footer .left');
       footer.innerHTML = `This website is using <a href="${URLs.repo}" target="_blank">Enjin Enhacer</a>`;
+    }
+    
+    /* OPTIONAL ENHACEMENTS */
+    this.enhaceTags = function() {
+      if (TAGS.indexOf(enhacements) >= 0) {
+        document.body.classList.add('EnjinEnhacer--tags');
+      }
+    }
+
+    // Remove 4 layers that are blocking Flash elements to be clickable (and playable) on website's header
+    this.unblockFlashHeader = function() {
+      if (FLASH_HEADER.indexOf(this.enhacements) >= 0) {
+        var elem;
+        elem = document.querySelector('.special_container .m_header .tl');
+        if (elem && elem.remove) elem.remove();
+
+        elem = document.querySelector('.special_container .m_header .tr');
+        if (elem && elem.remove) elem.remove();
+
+        elem = document.querySelector('.special_container .m_header .bl');
+        if (elem && elem.remove) elem.remove();
+
+        elem = document.querySelector('.special_container .m_header .br');
+        if (elem && elem.remove) elem.remove();
+      }
+    }
+
+    // Complete wowprogress.com link to our guild's profile
+    this.enhaceWowprogressLink = function() {
+      if (WOWPROGRESS_LINK.indexOf(this.enhacements) >= 0){
+        var wowprogressLink = document.querySelector('.wowprogress_link a');
+        if (wowprogressLink) {
+          wowprogressLink.href += `guild/${that.region}/${that.parseRealm(that.realm)}/${that.guild}`;
+          wowprogressLink.target = '_blank';
+        }
+      }
     }
   };
 
@@ -86,9 +136,12 @@
 
     // If body is available, so we can mount things on it
     if (document.body !== null && typeof document.body !== undefined) {
-      enjinEnhacer.unblockFlashHeader();
       enjinEnhacer.increaseMicroTags();
       enjinEnhacer.enhacedLink();
+
+      enjinEnhacer.enhaceTags();
+      enjinEnhacer.unblockFlashHeader();
+      enjinEnhacer.enhaceWowprogressLink();
 
     // If body is not available
     } else {
